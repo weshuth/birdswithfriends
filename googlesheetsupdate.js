85686dafc0a4a18f8a0ea4ec6622772f5141ec85
weshuth <104175663+weshuth@users.noreply.github.com>
2022-04-21 22:02:04 -0400

    Update googlesheetsupdate.js

--- googlesheetsupdate.js
+++ googlesheetsupdate.js
@@ -1,9 +1,11 @@
 var SHEET_ID = '16tY-_-V0LHV9R3iUXD4G-KAsJkRgqGhx83Q3CEveQgs';
-var ACCESS_TOKEN = 'ya29.A0ARrdaM_yjYLPFtz2eeLCdgFJuyHVZ3DrU4li2BNZdfu1lhKn0i3uQCrGAK7kQN3x-PKW4qXba2RQFhFcr2hFEMCU7IvsrnRnl1p8S_5GIXaiXg9ItHtCTWgzUWr6VN6HJNg2v6eNg5Va4lmV9twO7KuxmxKj';
-
+var ACCESS_TOKEN = 'ya29.A0ARrdaM8BnSUWEpMAIiIShCcyU5OOHYLsjSHBsj0q5tLX2M0v0ES5mu5t3hiVTs-2iP1e9Zgx4BQxmZrSM0uIvrySLav785FEWCfySMida-hO-W-MY4rCZIe2MCtm5pUGil1xE-YN2ZdkAU9OkW8kcIQKuGXC
+var birdName = document.querySelector("input[list='birds']").value;
+var birdCount = document.querySelector("input[id='bcount']").value;
 
 
 var updateSheetValues = () => {
+    console.log("updating bird count");
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
       method: "POST",
       headers: {
@@ -19,13 +21,13 @@ var updateSheetValues = () => {
         "range": {
           "startColumnIndex": 0,
           "endColumnIndex": 1,
-          "startRowIndex": 0,
-          "endRowIndex": 1,
+          "startRowIndex": 1,
+          "endRowIndex": 2,
           "sheetId": 0
         },
         "cell": {
           "userEnteredValue": {
-            "numberValue": 60
+            "numberValue": birdCount
           }
         },
         "fields": "*"

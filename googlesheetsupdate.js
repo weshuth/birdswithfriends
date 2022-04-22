c9278a2f2df259b9bca3457ea198dd8f398b48bc
weshuth <104175663+weshuth@users.noreply.github.com>
2022-04-21 21:28:11 -0400

    Update googlesheetsupdate.js

--- googlesheetsupdate.js
+++ googlesheetsupdate.js
@@ -1,12 +1,19 @@
-//let data = {element: "barium"};
-document.querySelectorAll("button").addEventListener("click",function(){
-fetch("https://sheets.googleapis.com/v4/spreadsheets/16tY-_-V0LHV9R3iUXD4G-KAsJkRgqGhx83Q3CEveQgs:batchUpdate", {
-  method: "POST",
-  headers: {'Content-Type': 'application/json', 
-  Authorization: "Bearer ya29.A0ARrdaM_yjYLPFtz2eeLCdgFJuyHVZ3DrU4li2BNZdfu1lhKn0i3uQCrGAK7kQN3x-PKW4qXba2RQFhFcr2hFEMCU7IvsrnRnl1p8S_5GIXaiXg9ItHtCTWgzUWr6VN6HJNg2v6eNg5Va4lmV9twO7KuxmxKj",
-   },
-  body: JSON.stringify(        
-    requests: [{
+var SHEET_ID = '16tY-_-V0LHV9R3iUXD4G-KAsJkRgqGhx83Q3CEveQgs';
+var ACCESS_TOKEN = 'ya29.A0ARrdaM_yjYLPFtz2eeLCdgFJuyHVZ3DrU4li2BNZdfu1lhKn0i3uQCrGAK7kQN3x-PKW4qXba2RQFhFcr2hFEMCU7IvsrnRnl1p8S_5GIXaiXg9ItHtCTWgzUWr6VN6HJNg2v6eNg5Va4lmV9twO7KuxmxKj';
+
+
+
+var updateSheetValues = () => {
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //update this token with yours. 
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({
+
+        requests: [{
           repeatCell: {
             range: {
               startColumnIndex: 0,
@@ -25,7 +32,5 @@ fetch("https://sheets.googleapis.com/v4/spreadsheets/16tY-_-V0LHV9R3iUXD4G-KAsJk
         }]
 
       })
-}).then(res => {
-  console.log("Request complete! response:", res);
-});
-}
+    })
+  }
